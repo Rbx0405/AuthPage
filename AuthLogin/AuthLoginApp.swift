64fb50ac63256bc6553417eec9dab8b911dd9c47
Rishit <rishitbutola10@gmail.com>
2024-08-23 20:08:56 +0530

    Initial Commit

---
+++ AuthLogin/AuthLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthLoginApp.swift
+//  AuthLogin
+//
+//  Created by Rishit Butola on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AuthLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthLogin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthLogin
+//
+//  Created by Rishit Butola on 23/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

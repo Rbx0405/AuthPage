f3275dc8b44af10587b731ca98e55ad1ef426706
Rishit <rishitbutola10@gmail.com>
2024-08-24 10:33:29 +0530

    Login complete

--- AuthLogin/View/Extension/String.swift
+++
@@ -1,8 +0,0 @@
-//
-//  String.swift
-//  AuthLogin
-//
-//  Created by Rishit Butola on 23/08/24.
-//
-
-import Foundation
--- AuthLogin/View/LoginView.swift
+++ AuthLogin/View/LoginView.swift
@@ -8,8 +8,101 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email:String=""
+    @State private var password:String=""
+    
+    private func isValidPassword(_ password:String){
+        //min 6 characters
+        //min 1 uppercase
+        //min 1 special character
+        
+        let passwordRegex = NSPredicate(format: " MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])(?=.*[A-Z]).{6,}$")
+        
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack{
+                HStack{
+                    Text("Welcome Back!")
+                        .font(.largeTitle)
+                        .bold()
+                    
+                    Spacer()
+                    
+                }
+                .padding()
+                .padding(.top)
+                
+                Spacer()
+                
+                
+                HStack{
+                    Image(systemName: "mail")
+                    TextField("Email",text: $email)
+                    
+                    Spacer()
+                    
+                    
+                    if(email.count != 0 ){
+                        
+                        Image(systemName: "checkmark")
+                            .fontWeight(.bold)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        
+                    }
+                    
+                    
+                }
+                .padding()
+                .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3.0)
+                )
+                .padding()
+                
+                HStack{
+                    Image(systemName: "lock")
+                    SecureField("Password",text: $password)
+                    
+                    Spacer()
+                    
+                    if (password.count != 0){
+                        
+                        Image(systemName: email.isValidEmail() ? "checkmark" : "xmark")
+                            .fontWeight(.bold)
+                            .foregroundColor(email.isValidEmail() ? .green : .red)
+                        
+                    }
+                    
+                    
+                    
+                }
+                .padding()
+                .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3.0)
+                )
+                .padding()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/){
+                    Text("Don't have an Account?")
+                        .bold()
+                }
+                
+                Spacer()
+                Spacer()
+                
+                Button(action:{}){
+                    Text("Sign In")
+                        .bold()
+                }
+                
+                
+                
+            }
+        }
     }
 }
 
